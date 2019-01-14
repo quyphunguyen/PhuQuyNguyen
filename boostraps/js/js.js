cbf57a18dc15cbe6107d1a575722d2f04abca47e
quynguyen <npquy.981201@gmail.com>
2019-01-14 22:26:39 +0700

    quy

---
+++ boostraps/js/js.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".nav a").click(function(){
+    $(this).tab('show');
+  });
+
+  $(".about").click(function () {
+			// $('.about').animate({
+				// $('#next').show(1000);
+			// })
+			// $('#next').css("top","0px");
+		})
+
+});
